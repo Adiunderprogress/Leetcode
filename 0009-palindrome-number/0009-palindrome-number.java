0b38792b6712f26de10a341a2fe453ce21a99974
Aditya Kumar <adi123tya456kumar@gmail.com>
2026-07-14 11:22:47 +0530

    Time: 5 ms (80.61%), Space: 45.7 MB (94.52%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        // Edge cases: 
-        // 1. Negative numbers are not palindromes (e.g., -121 reads as 121-)
-        // 2. Numbers ending in 0 are not palindromes, except 0 itself (e.g., 10, 100)
+        // Special cases:
+        // As discussed above, when x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, for it to be a palindrome,
+        // the first digit of the number also needs to be 0. Only 0 satisfies this.
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 
         int reversedHalf = 0;
-        // Keep reversing the digits of the second half until it meets or exceeds the first half
         while (x > reversedHalf) {
             reversedHalf = (reversedHalf * 10) + (x % 10);
             x /= 10;
         }
 
-        // For even length: x == reversedHalf (e.g., 1221 becomes x = 12, reversedHalf = 12)
-        // For odd length: x == reversedHalf / 10 to get rid of the middle digit (e.g., 12321 becomes x = 12, reversedHalf = 123)
+        // When the length is an odd number, we can get rid of the middle digit by reversedHalf/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12, reversedHalf = 123,
+        // since the middle digit doesn't matter in palindrome, we can simply get rid of it.
         return x == reversedHalf || x == reversedHalf / 10;
     }
 }
